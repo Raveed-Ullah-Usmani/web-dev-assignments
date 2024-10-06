7aa095c9a4b0e25cda0b4cb8c90e0485ed6f22a0
Raveed <raveedusmani@gmail.com>
2024-10-06 19:43:33 +0500

    Pages directory add and update

--- assignment-2/react-components-ui/coffee-master/src/App.jsx
+++ assignment-2/react-components-ui/coffee-master/src/App.jsx
@@ -1,24 +1,10 @@
-import Header from './components/Header';
-import Banner from './components/Banner';
-import VideoSection from './components/VideoSection';
-import MenuSection from './components/MenuSection';
-import GallerySection from './components/GallerySection';
-import ReviewSection from './components/ReviewSection';
-import BlogSection from './components/BlogSection';
-import Footer from './components/Footer';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Banner />
-      <VideoSection />
-      <MenuSection />
-      <GallerySection />
-      <ReviewSection />
-      <BlogSection />
-      <Footer />
-    </div>
+    <>
+      <Home />
+    </>
   );
 };
 
---
+++ assignment-2/react-components-ui/coffee-master/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import VideoSection from '../components/VideoSection';
+import MenuSection from '../components/MenuSection';
+import GallerySection from '../components/GallerySection';
+import ReviewSection from '../components/ReviewSection';
+import BlogSection from '../components/BlogSection';
+import Footer from '../components/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Banner />
+            <VideoSection />
+            <MenuSection />
+            <GallerySection />
+            <ReviewSection />
+            <BlogSection />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
